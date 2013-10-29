1a92a251f60af3e0498774c5a8b287d99ec0d2ef
Joe Pangallo <jpangall@gmail.com>
2013-10-29 12:31:00 -0500

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,6 +1,6 @@
 <?php
 // defines database connection data
-define('DB_HOST', 'mysql:host=leddsoftwarecom.ipagemysql.com;dbname=_cust1_');
+define('DB_HOST', '');
 define('DB_USER', 'jpangall');
 define('DB_PASSWORD', '_Iwboo16_');
 define('CSSERROR','required');
