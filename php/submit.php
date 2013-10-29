95098ad8d091602eb6109d8a6ff601c2971cde81
Joe Pangallo <jpangall@gmail.com>
2013-10-29 12:27:59 -0500

    Update submit.php

--- php/submit.php
+++ php/submit.php
@@ -1,5 +1,5 @@
 <?php  
-		//session_save_path( "/home/users/web/b46/ipg.leddsoftwarecom/cgi-bin/tmp" );
+		
 		session_start();
 		require_once ('cvalpersonal.php');  
 		require_once ('cvalemployment.php');  
@@ -7,15 +7,7 @@
 		require_once ('cBaseAggregator.php'); 
         require_once ('cLeadFlash.php'); 
 		require_once ('config.php');
-        
-        /*try
-        {
-           $pdo = new PDO('mysql:host=leddsoftwarecom.ipagemysql.com;dbname=_cust1_','jpangall', '_Iwboo16_');    
-        }
-         catch ( PDOException $e )
-        {
-           echo $e->getMessage();  
-        }  */                               
+                                
 				  
 		$valPersonal = new CValPersonalInfo();
 		$valPersonal->ValidatePersonal();
